f688af49fd7139e9cd04a446f81369236c146a24
Instinct29 <89262248+Instinct29@users.noreply.github.com>
2022-06-30 18:44:48 +0530

    Did some changes in Adv react section/ Redux remaining and solution for Adv.react remaining

--- src/assignments/AdvReactTheory/AdvReact.js
+++ src/assignments/AdvReactTheory/AdvReact.js
@@ -1,8 +1,29 @@
 import React from 'react'
+import NavBar from '../../components/navBar/NavBar'
 
 const AdvReact = () => {
   return (
-    <div>AdvReact</div>
+    <>
+        <NavBar /> 
+        <div className='container'>
+          <h2 className='l'>Questions</h2>
+          <ol>
+            <li className='l'>Code Splitting ?</li>
+            <li className='l'>Error-Boundary ?</li>
+            <li className='l'>Context ?</li>
+            <li className='l'>Refs and DOM ?</li>
+            <li className='l'>Strict Mode ?</li>
+            <li className='l'>Type checking with prop types ?</li>
+            <li className='l'>Higher order components ?</li>
+            <li className='l'>Forwarding Refs ?</li>
+          </ol>
+          <h2 className='l'>Answers</h2>
+
+        </div>
+        
+    </>
+    
+
   )
 }
 
